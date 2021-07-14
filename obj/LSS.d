3f2112be210165f2c16e740929f8206b9e66377b
CrysoK <57516378+CrysoK@users.noreply.github.com>
2021-07-13 22:50:57 -0300

    .d

---
+++ obj/LSS.d
@@ -0,0 +1,2 @@
+LSS.o: src/include/LSS.c src/include/LSS.h src/include/sds/sds.h \
+ src/include/auxlib.h
---
+++ obj/main.d
@@ -0,0 +1,2 @@
+main.o: src/main.c src/include/auxlib.h src/include/LSS.h \
+ src/include/sds/sds.h src/include/sds/sds.h
